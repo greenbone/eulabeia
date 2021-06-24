97b58a55f6399f1dd413f5579ce1c88b23047b66
Philipp Eder <philipp.eder@greenbone.net>
2021-06-24 16:57:49 +0200

    Refactored and documented SetValueOf

--- models/generic_test.go
+++ models/generic_test.go
@@ -13,25 +13,25 @@ func TestSetValueOf(t *testing.T) {
 	var tests = []struct {
 		name     string
 		val      interface{}
-		err      error
+		err      string
 		expected interface{}
 	}{
 		{
 			"A",
 			"b",
-			nil,
+			"",
 			TestStruct{"b", 0},
 		},
 		{
 			"B",
 			"b",
-			errInvalidValueKind,
+			"field type (int) does not match value type (string)",
 			TestStruct{"b", 0},
 		},
 		{
 			"C",
 			"b",
-			errInvalidField,
+			"field (C) not found on target (models.TestStruct)",
 			TestStruct{"b", 0},
 		},
 	}
@@ -39,10 +39,12 @@ func TestSetValueOf(t *testing.T) {
 		target := &TestStruct{"a", 0}
 		err := SetValueOf(target, test.name, test.val)
 
-		if err != test.err {
-			t.Errorf("[%d] returned %v but expected %v", i, err, test.err)
+		if err != nil {
+			if test.err == "" || test.err != err.Error() {
+				t.Errorf("[%d] returned '%s' but expected '%v'", i, err, test.err)
+			}
 		}
-		if test.err == nil {
+		if test.err == "" {
 			if test.expected != *target {
 				t.Errorf("[%d] returned %v but expected %v", i, target, test.expected)
 
