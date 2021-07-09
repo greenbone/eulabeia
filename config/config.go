4ac04540445d04f83bf7fe721cff92f0ac4bf18f
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-07-09 13:56:14 +0200

    Use marshal, use interface, define Config struct

--- config/config.go
+++ config/config.go
@@ -1,70 +1,31 @@
 package config
 
-import (
-	"errors"
-	"io/ioutil"
-	"os"
-
-	"github.com/pelletier/go-toml"
-)
-
-// Check if file exists
-// Returns true if file exists, false else
-func fileExists(path string) bool {
-	if _, err := os.Stat(path); err == nil {
-		// file exists
-		return true
-	}
-	return false
+type Certificate struct {
+	DefaultKeyFile  string // Path to the default location of the private Key
+	DefaultCertFile string // Path to the default location of the private Cert
 }
 
-// Look up the config file in the order
-// 1. given by parameter --config
-// 2. custom user config file in home
-// 3. in /usr/etc or /etc/ ...
-// Returns the path to the first found file
-func findConfigFile(path string) string {
-	if path != "" && fileExists((path)) {
-		return path
-	}
-	// look in the default paths
-	var defaultPaths = [...]string{
-		os.Getenv("HOME") + "/.config",
-		"/usr/etc",
-		"/etc",
-	}
-	for _, path := range defaultPaths {
-		path += "/eulabeia/config.toml"
-		if fileExists(path) {
-			// file exists
-			return path
-		}
-	}
-	panic(errors.New("no config file found"))
+type Connection struct {
+	Server  string	// Bind address of server in format 133.713.371.337:1337
+	Timeout int64	// TODO
 }
 
-func Overwrite(config_map *toml.Tree, key string, value string) {
-	//TODO overwrite at runtime given arguments
-	config_map.Set(key, value)
+type ScannerPreferences struct {
+	scanInfoStoreTime int64 // TODO
+	maxScan           int64 // TODO
+	maxQueuedScans    int64 // Maximum number of scans that can be queued
 }
 
-// Load the config file after startup
-// In the returned config map config values can
-// be accessed with Get("section.key").(string)
-// The values can also be overwritten at runtime
-// Returns the config map
-func Load(path string) *toml.Tree {
-	config_path := findConfigFile(path)
-
-	// Read the config file
-	byte, err := ioutil.ReadFile(config_path)
-	if err != nil {
-		panic(err)
-	}
-
-	tree, err := toml.Load(string(byte))
-	if err != nil {
-		panic(err)
-	}
-	return tree
+type Preferences struct {
+	LogLevel string // Loglevel (Debug, Info ...)
+	LogFile  string // Path to logfile
+	Niceness int64  // TODO
 }
+
+type Configuration struct {
+	Certificate        Certificate
+	Connection         Connection
+	ScannerPreferences ScannerPreferences
+	Preferences        Preferences
+}
+
---
+++ config/config_handler.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"reflect"
+
+	"github.com/pelletier/go-toml"
+)
+
+type configurationHandler interface {
+	Load(string)
+}
+
+type ConfigurationHandler struct {
+	Configuration Configuration // The configuration object
+	module        string        // The module name
+	path          string        // Path to the configuration file
+}
+
+// Check if file exists
+// Returns true if file exists, false else
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		// file exists
+		return true
+	}
+	return false
+}
+
+// Look up the config file in the order
+// 1. given by parameter --config
+// 2. custom user config file in home
+// 3. in /usr/etc or /etc/ ...
+// Returns the path to the first found file
+func findConfigFile(path string, module string) string {
+	if path != "" && fileExists((path)) {
+		return path
+	}
+	// look in the default paths
+	var defaultPaths = [...]string{
+		os.Getenv("HOME") + "/.config",
+		"/usr/etc",
+		"/etc",
+	}
+	for _, path := range defaultPaths {
+		path += "/" + module + "/config.toml"
+		if fileExists(path) {
+			// file exists
+			return path
+		}
+	}
+	panic(errors.New("no config file found"))
+}
+
+// Load the config file after startup
+func (c *ConfigurationHandler) Load(path string, module string) {
+	c.module = module
+	c.Configuration = Configuration{}
+	c.path = findConfigFile(path, module)
+
+	// Read the config file
+	bytes, err := ioutil.ReadFile(c.path)
+	if err != nil {
+		panic(err)
+	}
+
+	toml.Unmarshal(bytes, c.Configuration)
+}
