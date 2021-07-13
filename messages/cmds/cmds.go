52e5660b55bb291e2578d57c4d8ff530d3184e91
Philipp Eder <philipp.eder@greenbone.net>
2021-07-13 13:55:09 +0200

    Add New function for cmds
    To make it easier to create cmds New function for each cmd is provided.

--- messages/cmds/cmds.go
+++ messages/cmds/cmds.go
@@ -1,6 +1,8 @@
 package cmds
 
 import (
+	"fmt"
+
 	"github.com/greenbone/eulabeia/messages"
 )
 
@@ -24,20 +26,65 @@ type Create struct {
 	messages.Message
 }
 
+func buildMessageType(function string, aggregate string, destination string) string {
+	result := fmt.Sprintf("%s.%s", function, aggregate)
+	if destination != "" {
+		result = fmt.Sprintf("%s.%s", result, destination)
+	}
+	return result
+}
+
+// NewCreate creates a new Create cmd
+func NewCreate(aggregate string, destination string, groupID string) Create {
+	return Create{
+		Message: messages.NewMessage(buildMessageType("create", aggregate, destination), "", groupID),
+	}
+}
+
 // Get is used by a client to get the latest snapshot of an aggregate.
 //
 // The response for Get is usually the aggragte with Message information and can be found within a model.
 type Get IDCMD
+
+// NewGet creates a new Get cmd
+func NewGet(aggregate string, id string, destination string, groupID string) Get {
+	return Get{
+		Identifier: messages.Identifier{
+			ID:      id,
+			Message: messages.NewMessage(buildMessageType("get", aggregate, destination), "", groupID),
+		},
+	}
+}
 
 // Delete is used by a client to delete the latest snapshot of an aggregate.
 //
 // The response of Delete is Deleted.
 type Delete IDCMD
 
+// NewDelete creates a new Delete cmd
+func NewDelete(aggregate string, id string, destination string, groupID string) Delete {
+	return Delete{
+		Identifier: messages.Identifier{
+			ID:      id,
+			Message: messages.NewMessage(buildMessageType("delete", aggregate, destination), "", groupID),
+		},
+	}
+}
+
 // Start indicates that something with the ID should be started.
 //
 // As an example an event with the stype start.scan with the id 1 would start scan id 1
 type Start IDCMD
+
+// NewStart creates a new Start cmd
+func NewStart(aggregate string, id string, destination string, groupID string) Start {
+	return Start{
+		Identifier: messages.Identifier{
+			ID:      id,
+			Message: messages.NewMessage(buildMessageType("start", aggregate, destination), "", groupID),
+		},
+	}
+}
 
 // Modify indicates that a entity should be modified.
 //
@@ -47,4 +94,15 @@ type Modify struct {
 	eventType
 	messages.Identifier
 	Values map[string]interface{} `json:"values"`
+}
+
+// NewModify creates a new Modify cmd
+func NewModify(aggregate string, id string, valus map[string]interface{}, destination string, groupID string) Modify {
+	return Modify{
+		Identifier: messages.Identifier{
+			ID:      id,
+			Message: messages.NewMessage(buildMessageType("modify", aggregate, destination), "", groupID),
+		},
+		Values: valus,
+	}
 }
