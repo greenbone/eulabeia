608ed8b8e10c856358d3244bebba23109cbdfc2a
Philipp Eder <philipp.eder@greenbone.net>
2021-08-12 19:15:50 +0200

    Change: json.h documentation.

--- c/libeulabeia/include/eulabeia/json.h
+++ c/libeulabeia/include/eulabeia/json.h
@@ -2,7 +2,16 @@
 #define __EULABEIA_JSON_H
 #include <eulabeia/types.h>
 #include <json-glib/json-glib.h>
-
+/*
+ * @brief inititalizes JsonNode and JsonObject.
+ *
+ * @param[out] j_node; the JsonNode to be initialized. The caller is responsible
+ * for j_node.
+ * @param[out] j_obj; the JsonObject to be initialized based on the j_node. The
+ * caller is responsible for j_obj.
+ * @return 0 on success; -1 when payload is empty; -2 when payload is not a
+ * json; -3 when payload is not a json object.
+ */
 int eulabeia_json_object(const char *payload,
 			 JsonNode **j_node,
 			 JsonObject **j_obj);
@@ -26,17 +35,115 @@ int eulabeia_json_object(const char *payload,
  */
 int eulabeia_json_message(JsonObject *obj, struct EulabeiaMessage **msg);
 
+/*
+ * @brief parses already initialized JsonObject to EulabeiaFailure
+ *
+ * This function expects an already initialized JsonObject. To initialize one
+ * you can call eulabeia_json_jsonobject.
+ *
+ * @param[in] obj, the JsonObject to be parsed
+ * @param[in] msg, the EulabeiaMessage to be included into failure
+ * @param[out] failure, the EulabeiaFailure this function will allocate the
+ * memory. The caller is responsible for cleaning.
+ *
+ * @return 0 on success,
+ * @return 0 on success,
+ *  -1 on msg is NULL
+ *  -2 on msg type is incorrect
+ *  -3 on JsonObject is incorrect
+ *  -4 on allocation failure
+ *  -5 on missing ID
+ *  -6 on failure to set value
+ */
 int eulabeia_json_failure(JsonObject *obj,
 			  struct EulabeiaMessage *msg,
 			  struct EulabeiaFailure **failure);
+/*
+ * @brief parses already initialized JsonObject to EulabeiaStatus
+ *
+ * This function expects an already initialized JsonObject. To initialize one
+ * you can call eulabeia_json_jsonobject.
+ *
+ * @param[in] obj, the JsonObject to be parsed
+ * @param[in] msg, the EulabeiaMessage to be included into status
+ * @param[out] status, the EulabeiaStatus this function will allocate the
+ * memory. The caller is responsible for cleaning.
+ *
+ * @return 0 on success,
+ *  -1 on msg is NULL
+ *  -2 on msg type is incorrect
+ *  -3 on JsonObject is incorrect
+ *  -4 on allocation failure
+ *  -5 on missing ID
+ *  -6 on failure to set value
+ */
 int eulabeia_json_status(JsonObject *obj,
 			 struct EulabeiaMessage *msg,
 			 struct EulabeiaStatus **status);
+/*
+ * @brief parses already initialized JsonObject to EulabeiaHosts
+ *
+ * This function expects an already initialized JsonObject. To initialize one
+ * you can call eulabeia_json_jsonobject.
+ *
+ * @param[in] obj, the JsonObject to be parsed
+ * @param[out] hosts, the EulabeiaHosts this function will allocate the memory.
+ * The caller is responsible for cleaning.
+ *
+ * @return 0 on success,
+ *  -1 on invalid JsonObject
+ *  -2 on host allocation failure
+ *  -3,-4 on setting value failure
+ */
 int eulabeia_json_hosts(JsonArray *arr, struct EulabeiaHosts **hosts);
+/*
+ * @brief parses already initialized JsonObject to EulabeiaPlugins
+ *
+ * This function expects an already initialized JsonObject. To initialize one
+ * you can call eulabeia_json_jsonobject.
+ *
+ * @param[in] obj, the JsonObject to be parsed
+ * @param[out] plugins, the EulabeiaPlugins this function will allocate the
+ * memory. The caller is responsible for cleaning.
+ *
+ * @return 0 on success,
+ *  -1 on invalid JsonObject
+ *  -2 on plugin allocation failure
+ *  -3,-4 on setting value failure
+ */
 int eulabeia_json_plugins(JsonArray *arr, struct EulabeiaPlugins **plugins);
+/*
+ * @brief parses already initialized JsonObject to EulabeiaPorts
+ *
+ * This function expects an already initialized JsonObject. To initialize one
+ * you can call eulabeia_json_jsonobject.
+ *
+ * @param[in] obj, the JsonObject to be parsed
+ * @param[out] ports, the EulabeiaPorts this function will allocate the memory.
+ * The caller is responsible for cleaning.
+ *
+ * @return 0 on success,
+ *  -1 on invalid JsonObject
+ *  -2 on port allocation failure
+ *  -3,-4 on setting value failure
+ */
 int eulabeia_json_ports(JsonArray *arr, struct EulabeiaPorts **ports);
+/*
+ * @brief transforms EulabeiaScan to json string.
+ *
+ * @param[in] msg, the EulabeiaMessage to include
+ * @param[in] scan, the scan to transform to json string.
+ * @return a json char array or NULL on failure.
+ */
 char *eulabeia_scan_message_to_json(const struct EulabeiaMessage *msg,
 				    const struct EulabeiaScan *scan);
+/*
+ * @brief transforms EulabeiaTarget to json string.
+ *
+ * @param[in] msg, the EulabeiaMessage to include
+ * @param[in] target, the target to transform to json string.
+ * @return a json char array or NULL on failure.
+ */
 char *eulabeia_target_message_to_json(const struct EulabeiaMessage *msg,
 				      const struct EulabeiaTarget *target);
 #endif
