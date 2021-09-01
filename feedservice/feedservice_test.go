ffb629a5dc6f2c8ff2a9107e3f346bc5e9b7dc3e
Christoph Kraemer <christoph.kraemer@greenbone.net>
2021-09-01 19:22:25 +0200

    unittests for resolving vt groups

--- feedservice/feedservice_test.go
+++ feedservice/feedservice_test.go
@@ -153,6 +153,35 @@ func (rm RedisMock) GetList(db int, key string, start int, end int) ([]string, e
 			"test_family",
 			"name",
 		}, nil
+	case "nvt:test2":
+		return []string{
+			"filename2.nasl",
+			"required_key1, required_key2",
+			"mandatory_key1, mandatory_key2",
+			"excluded_key1, excluded_key2",
+			"22, 80",
+			"42, 400",
+			"dependency1.nasl, dependency2.nasl",
+			"cvss_base_vector=AV:N/AC:L/Au:N/C:P/I:P/A:P|" +
+				"last_modification=0|" +
+				"creation_date=0|" +
+				"summary=test summary|" +
+				"vuldetect=test vulnerability|" +
+				"impact=no impact|" +
+				"insight=this is only for testing|" +
+				"affected=this test|" +
+				"solution=none|" +
+				"solution_method=foo|" +
+				"solution_type=vendorfix|" +
+				"qod_type=test",
+			"CVE-2000-0003, CVE-2000-0004",
+			"test_bid",
+			"URL:https://example.org, Advisory-ID:testID",
+			"1",
+			"0",
+			"foo_bar_family",
+			"name2",
+		}, nil
 	default:
 		return nil, fmt.Errorf("unknown key")
 	}
@@ -160,7 +189,8 @@ func (rm RedisMock) GetList(db int, key string, start int, end int) ([]string, e
 
 func (rm RedisMock) GetKeys(db int, filter string) ([]string, error) {
 	return []string{
-		"test",
+		"nvt:test",
+		"nvt:test2",
 	}, nil
 }
 
@@ -189,4 +219,124 @@ func TestGetVT(t *testing.T) {
 	if string(vtTestJSON) != string(vtJSON) {
 		t.Fatalf("vtTestJSON != vtJSON\n\nvtTestJSON:\n %s\n\n vtJSON:\n%s\n", vtTestJSON, vtJSON)
 	}
+}
+
+func TestResolveFilter(t *testing.T) {
+	fs := feed{
+		mqtt:    MockPubSub{},
+		context: "",
+		rc:      &RedisMock{},
+	}
+	var oids []string
+	var err error
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "family",
+			Value: "foo_bar_family",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve family: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test2" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test2", oids[0])
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "category",
+			Value: "1",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve category: %s", err)
+	}
+	if len(oids) != 2 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 2, len(oids))
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "tag",
+			Value: "solution_type=noFix",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve tag: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test", oids[0])
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "cve",
+			Value: "CVE-2000-0003",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve cve: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test2" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test2", oids[0])
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "name",
+			Value: "name2",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve name: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test2" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test2", oids[0])
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "filename",
+			Value: "filename2.nasl",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve filename: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test2" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test2", oids[0])
+	}
+
+	oids, err = fs.ResolveFilter([]models.VTFilter{
+		{
+			Key:   "bid",
+			Value: "test_bid",
+		},
+	})
+	if err != nil {
+		t.Errorf("Unable to resolve bid: %s", err)
+	}
+	if len(oids) != 1 {
+		t.Fatalf("wrong number of oids: expected %d, got %d", 1, len(oids))
+	}
+	if oids[0] != "test2" {
+		t.Errorf("Got wrong OID: expected %s, got %s", "test2", oids[0])
+	}
+
 }
